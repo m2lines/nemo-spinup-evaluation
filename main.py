ef2ec4f1f4a1bd1fc3a443945e048add0d9ba4dc
Matt Archer <ma595@cam.ac.uk>
2025-04-29 20:25:56 +0100

    Amend most of metrics.py ruff linting errors
    More style changes

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
-"Run metrics on data file"
+"""Run metrics on data file."""
 
 import os
+
 import xarray as xr
 
 # from utils import get_density, get_depth
 from metrics import (
+    ACC_Drake_metric,
+    NASTG_BSF_max,
     check_density,
     temperature_500m_30NS_metric,
     temperature_BWbox_metric,
     temperature_DWbox_metric,
-    ACC_Drake_metric,
-    NASTG_BSF_max,
 )
 
 
 def read_data(datafilepath, maskfilepath):
+    """Read the data and mask files."""
     filename = os.path.basename(datafilepath)
     if "restart" in filename:
         print("The provided file is a 'restart' file\n")
@@ -34,6 +36,20 @@ def read_data(datafilepath, maskfilepath):
 
 
 def apply_metrics(data, mask):
+    """
+    Apply metrics to the data and mask.
+
+    Parameters
+    ----------
+    data  : xarray.Dataset
+        The dataset containing ocean model variables.
+    mask  : (xarray.Dataset)
+        The dataset containing mask variables.
+
+    Returns
+    -------
+        dict: A dictionary containing the results of the metrics.
+    """
     metrics_dict = {
         "check_density": check_density,
         "temperature_500m_30NS_metric": temperature_500m_30NS_metric,
@@ -49,7 +65,7 @@ def apply_metrics(data, mask):
         try:
             results[name] = func(data, mask)
         except Exception as e:
-            results[name] = f"Error: {str(e)}"
+            results[name] = f"Error: {e!s}"
 
     print(f"Successfully ran the metric `{name}` on the given file")
 
