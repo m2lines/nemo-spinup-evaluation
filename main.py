c1995b05d600af75ae8eaee957c28e1abef52ed5
Surbhi Goel <surbhigoel77@gmail.com>
2025-05-14 10:29:53 +0100

    Added a results folder to hold eval output, formatted main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from src.metrics import (
     temperature_BWbox_metric,
     temperature_DWbox_metric,
 )
-from src.utils import (get_depth, get_density)
+from src.utils import get_depth, get_density
 
 from variable_aliases import VARIABLE_ALIASES, standardize_variables
 
@@ -26,11 +26,13 @@ def read_data(datafilepath, maskfilepath, variable_dict):
     print(f"The provided file is: {filename}\n")
 
     # Determine whether to decode CF conventions
-    decode_cf = not any(key in filename for key in ["grid_T", "grid_T_sampled", "grid_U", "grid_V"])
+    decode_cf = not any(
+        key in filename for key in ["grid_T", "grid_T_sampled", "grid_U", "grid_V"]
+    )
 
     # Open the dataset
     data = xr.open_dataset(datafilepath, decode_cf=decode_cf)
-        
+
     data = standardize_variables(data, variable_dict)
 
     # Open and standardize the mesh mask
@@ -61,12 +63,24 @@ def apply_metrics_restart(data, mask):
         "check_density_from_file": lambda d: check_density(d["density"][0]),
         ### Attention: Change restart with restart_updated ###
         ### Attention: I changed line 22 of utils.py, sshn-> ssh as the code works on renamed variables only. Needs to be discussed.
-        "check_density_computed": lambda d: check_density(get_density(d["temperature"], d["salinity"], get_depth(restart, mask),mask["tmask"])[0]),
-        "temperature_500m_30NS_metric": lambda d: temperature_500m_30NS_metric(d["temperature"][0], mask),
-        "temperature_BWbox_metric": lambda d: temperature_BWbox_metric(d["temperature"][0], mask),
-        "temperature_DWbox_metric": lambda d: temperature_DWbox_metric(d["velocity_zonal"][0], mask),
+        "check_density_computed": lambda d: check_density(
+            get_density(
+                d["temperature"], d["salinity"], get_depth(restart, mask), mask["tmask"]
+            )[0]
+        ),
+        "temperature_500m_30NS_metric": lambda d: temperature_500m_30NS_metric(
+            d["temperature"][0], mask
+        ),
+        "temperature_BWbox_metric": lambda d: temperature_BWbox_metric(
+            d["temperature"][0], mask
+        ),
+        "temperature_DWbox_metric": lambda d: temperature_DWbox_metric(
+            d["velocity_zonal"][0], mask
+        ),
         "ACC_Drake_metric": lambda d: ACC_Drake_metric(d["velocity_zonal"][0], mask),
-        "NASTG_BSF_max": lambda d: NASTG_BSF_max(d["velocity_meridional"][0], d["ssh"], mask),
+        "NASTG_BSF_max": lambda d: NASTG_BSF_max(
+            d["velocity_meridional"][0], d["ssh"], mask
+        ),
     }
 
     for name, func in metric_functions.items():
@@ -78,7 +92,10 @@ def apply_metrics_restart(data, mask):
 
     return results
 
-def apply_metrics_grid(data_grid_T, data_grid_T_sampled, data_grid_U, data_grid_V, restart, mask):
+
+def apply_metrics_grid(
+    data_grid_T, data_grid_T_sampled, data_grid_U, data_grid_V, restart, mask
+):
     """
     Apply metrics to the dataset and mask.
 
@@ -102,13 +119,24 @@ def apply_metrics_grid(data_grid_T, data_grid_T_sampled, data_grid_U, data_grid_
                 data_grid_T["temperature"],
                 data_grid_T["salinity"],
                 get_depth(restart, mask),
-                mask["tmask"]
-            )[0]),
-        "temperature_500m_30NS_metric": lambda: temperature_500m_30NS_metric(data_grid_T["temperature"], mask),
-        "temperature_BWbox_metric": lambda: temperature_BWbox_metric(data_grid_T["temperature"], mask),
-        "temperature_DWbox_metric": lambda: temperature_DWbox_metric(data_grid_U["velocity_zonal"], mask),
-        "ACC_Drake_metric": lambda: ACC_Drake_metric(data_grid_U["velocity_zonal"], mask),
-        "NASTG_BSF_max": lambda: NASTG_BSF_max(data_grid_V["velocity_meridional"], data_grid_T_sampled["ssh"], mask),
+                mask["tmask"],
+            )[0]
+        ),
+        "temperature_500m_30NS_metric": lambda: temperature_500m_30NS_metric(
+            data_grid_T["temperature"], mask
+        ),
+        "temperature_BWbox_metric": lambda: temperature_BWbox_metric(
+            data_grid_T["temperature"], mask
+        ),
+        "temperature_DWbox_metric": lambda: temperature_DWbox_metric(
+            data_grid_U["velocity_zonal"], mask
+        ),
+        "ACC_Drake_metric": lambda: ACC_Drake_metric(
+            data_grid_U["velocity_zonal"], mask
+        ),
+        "NASTG_BSF_max": lambda: NASTG_BSF_max(
+            data_grid_V["velocity_meridional"], data_grid_T_sampled["ssh"], mask
+        ),
     }
 
     for name, func in metric_functions.items():
@@ -122,7 +150,6 @@ def apply_metrics_grid(data_grid_T, data_grid_T_sampled, data_grid_U, data_grid_
             print(f"Error in metric {name}: {e}")
 
     return results
-
 
 
 def write_metric_results(results, output_filepath):
@@ -155,8 +182,7 @@ def write_metric_results(results, output_filepath):
             f.write(line + "\n")
             print(line)
 
-    print(f"\n✅ Successfully wrote metrics to '{output_filepath}'")
-
+    print(f"\n Successfully wrote metrics to '{output_filepath}'")
 
 
 if __name__ == "__main__":
@@ -203,16 +229,18 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
-    
+
     # Ensure at least one of the inputs is provided
-    if not args.restart and not (args.grid_T and args.grid_T_sampled and args.grid_U and args.grid_V):
-        print("Error: You must specify either --restart or all three grid files: --grid_T, --grid_U, and --grid_V.")
+    if not args.restart and not (
+        args.grid_T and args.grid_T_sampled and args.grid_U and args.grid_V
+    ):
+        print(
+            "Error: You must specify either --restart or all three grid files: --grid_T, --grid_U, and --grid_V."
+        )
         parser.print_help()
         sys.exit(1)
 
-
-
-  # Collect files paths based on arguments provided
+    # Collect files paths based on arguments provided
     data_files = []
     if args.restart:
         data_files.append(args.restart)
@@ -224,32 +252,49 @@ if __name__ == "__main__":
         data_files.append(args.grid_U)
     if args.grid_V:
         data_files.append(args.grid_V)
-        
-            
-        
+
     if args.restart and not args.grid_T:
         restart, mesh_mask = read_data(args.restart, args.mesh_mask, VARIABLE_ALIASES)
-        print(restart)
         results = apply_metrics_restart(restart, mesh_mask)
-        write_metric_results(results, "metrics_results_restart.txt")
-        print(results)
+        write_metric_results(results, "results/metrics_results_restart.txt")
     else:
         restart, mesh_mask = read_data(args.restart, args.mesh_mask, VARIABLE_ALIASES)
-        data_grid_T, mesh_mask = read_data(args.grid_T, args.mesh_mask, VARIABLE_ALIASES)
-        data_grid_T_sampled, mesh_mask = read_data(args.grid_T_sampled, args.mesh_mask, VARIABLE_ALIASES)
+        data_grid_T, mesh_mask = read_data(
+            args.grid_T, args.mesh_mask, VARIABLE_ALIASES
+        )
+        data_grid_T_sampled, mesh_mask = read_data(
+            args.grid_T_sampled, args.mesh_mask, VARIABLE_ALIASES
+        )
         data_grid_T_sampled["time_counter"] = data_grid_T["time_counter"]
-        data_grid_U, mesh_mask = read_data(args.grid_U, args.mesh_mask, VARIABLE_ALIASES)
-        data_grid_V, mesh_mask = read_data(args.grid_V, args.mesh_mask, VARIABLE_ALIASES)
-        results = apply_metrics_grid(data_grid_T, data_grid_T_sampled, data_grid_U, data_grid_V, restart, mesh_mask)
-        write_metric_results(results, "metrics_results_grid.txt")
-        print(results)
-        
-
+        data_grid_U, mesh_mask = read_data(
+            args.grid_U, args.mesh_mask, VARIABLE_ALIASES
+        )
+        data_grid_V, mesh_mask = read_data(
+            args.grid_V, args.mesh_mask, VARIABLE_ALIASES
+        )
+        results = apply_metrics_grid(
+            data_grid_T,
+            data_grid_T_sampled,
+            data_grid_U,
+            data_grid_V,
+            restart,
+            mesh_mask,
+        )
+        write_metric_results(results, "results/metrics_results_grid.txt")
 
 
 ##################################
 ### Command to run grid files ####
-##################################      
-# python3 main.py --restart ../nc_files/nc_files/DINO_00576000_restart.nc  --grid_T ../nc_files/nc_files/DINO_1y_grid_T.nc --grid_T_sampled .
-# ./nc_files/nc_files/DINO_1m_To_1y_grid_T.nc  --grid_U ../nc_files/nc_files/DINO_1y_grid_U.nc --grid_V ../nc_files/nc_files/DINO_1y_grid_V.nc  --mesh-mask ../nc_files/nc_files/mesh_mask.nc
- 
+##################################
+# python3 main.py --restart ../nc_files/nc_files/DINO_00576000_restart.nc
+#                 --grid_T ../nc_files/nc_files/DINO_1y_grid_T.nc
+#                 --grid_T_sampled ../nc_files/nc_files/DINO_1m_To_1y_grid_T.nc
+#                 --grid_U ../nc_files/nc_files/DINO_1y_grid_U.nc
+#                 --grid_V ../nc_files/nc_files/DINO_1y_grid_V.nc
+#                 --mesh-mask ../nc_files/nc_files/mesh_mask.nc
+
+##################################
+### Command to run restart files ####
+##################################
+# python3 main.py --restart ../nc_files/nc_files/DINO_00576000_restart.nc
+#                 --mesh-mask ../nc_files/nc_files/mesh_mask.nc
