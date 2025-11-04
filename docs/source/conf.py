19934a1bd02a775bd44992b37b6586ef3b3dc18b
Surbhi Goel <surbhigoel77@gmail.com>
2025-11-04 17:14:37 +0000

    Update latest source code location and general structural improvements

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,42 +1,32 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
+"""Sphinx configuration for Spinup-Evaluation documentation."""
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))  # assumes your code is in root
 
+sys.path.insert(0, os.path.abspath("../../src"))  # assumes your code is in root
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-project = 'Spinup-Evaluation'
-copyright = '2025, Surbhi Goel, Matt Archer'
-author = 'Surbhi Goel, Matt Archer'
-release = '2025'
+project = "Spinup-Evaluation"
+# ruff: noqa: A001
+copyright = "2025, Surbhi Goel, Matt Archer"
+author = "Surbhi Goel, Matt Archer"
+release = "2025"
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = [
-    'myst_parser',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
-    'sphinx_autodoc_typehints',
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx_autodoc_typehints",
 ]
 
 autosummary_generate = True
-
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
-language = 'English'
+language = "English"
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
