6eb200d42f6296352a08bafa112f9b8a2e1a7e4b
Matt Archer <ma595@cam.ac.uk>
2026-04-07 15:32:41 +0100

    update get-data.sh to pull from 'current' dir

--- tests/get-data.sh
+++ tests/get-data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -O https://thredds-su.ipsl.fr/thredds/fileServer/NEMO-SPINUP/evaluation/test-data/subsampled/DINO-simple-small.tar.gz
+curl -O https://thredds-su.ipsl.fr/thredds/fileServer/NEMO-SPINUP/evaluation/test-data/subsampled/current/DINO-simple-small.tar.gz
 echo "Extracting test data to tests/data"
 tar -xzf DINO-simple-small.tar.gz -C tests/data
 rm DINO-simple-small.tar.gz
